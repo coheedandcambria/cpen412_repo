2646f63bbc0c08287c7acbf0e96ba974781ca07a
haoweiyu94 <haoweiyu94@gmail.com>
2017-03-23 20:36:59 -0700

    Update IICRoutines.c

--- IICRoutines.c
+++ IICRoutines.c
@@ -10,6 +10,343 @@ void IIC_initialize(void){
 
 }
 
+void IIC_ReadByte(void){
+    int sr;
+    int mask, masked_n, tip_bit, ack_bit;
+    unsigned int value ;
+    unsigned char *IICPtr = (unsigned char *)(IICStart);
+    // check if TIP flag is negated
+    sr = IICPtr[0x04 << 1];
+    mask = 1 << 1;
+    masked_n = sr & mask;
+    tip_bit = masked_n >> 1;
+
+    while(tip_bit != 0){
+        sr = IICPtr[0x04 << 1];
+        masked_n = sr & mask;
+        tip_bit = masked_n >> 1;
+    }
+
+    // Writing to the transmit reg
+    IICPtr[0x03 << 1] = 0xA0;
+    IICPtr[0x04 << 1] = 0x91;
+
+    // check ack
+    sr = IICPtr[0x04 << 1];
+    mask = 1 << 7;
+    masked_n = sr & mask;
+    ack_bit = masked_n >> 7;
+
+    while(ack_bit != 0){
+        sr = IICPtr[0x04 << 1];
+        masked_n = sr & mask;
+        ack_bit = masked_n >> 7;
+    }
+
+    // check tip flag
+    sr = IICPtr[0x04 << 1];
+    mask = 1 << 1;
+    masked_n = sr & mask;
+    tip_bit = masked_n >> 1;
+
+    while(tip_bit != 0){
+        sr = IICPtr[0x04 << 1];
+        masked_n = sr & mask;
+        tip_bit = masked_n >> 1;
+    }
+
+    // specify the internal address
+    // by writing each byte of the addr to the transmit reg
+    IICPtr[0x03 << 1] = 0x00; // may need to update to be based on user input address
+    IICPtr[0x04 << 1] = 0x11; // set WR, IACK
+
+    // check ack
+    sr = IICPtr[0x04 << 1];
+    mask = 1 << 7;
+    masked_n = sr & mask;
+    ack_bit = masked_n >> 7;
+
+    while(ack_bit != 0){
+        sr = IICPtr[0x04 << 1];
+        masked_n = sr & mask;
+        ack_bit = masked_n >> 7;
+    }
+
+    // check tip flag
+    sr = IICPtr[0x04 << 1];
+    mask = 1 << 1;
+    masked_n = sr & mask;
+    tip_bit = masked_n >> 1;
+
+    while(tip_bit != 0){
+        sr = IICPtr[0x04 << 1];
+        masked_n = sr & mask;
+        tip_bit = masked_n >> 1;
+    }
+
+    // second byte of addr
+    IICPtr[0x03 << 1] = 0x00; // may need to update to be based on user input address
+    IICPtr[0x04 << 1] = 0x11; // set WR, IACK
+
+    // check ack
+    sr = IICPtr[0x04 << 1];
+    mask = 1 << 7;
+    masked_n = sr & mask;
+    ack_bit = masked_n >> 7;
+
+    while(ack_bit != 0){
+        sr = IICPtr[0x04 << 1];
+        masked_n = sr & mask;
+        ack_bit = masked_n >> 7;
+    }
+
+    // check tip flag
+    sr = IICPtr[0x04 << 1];
+    mask = 1 << 1;
+    masked_n = sr & mask;
+    tip_bit = masked_n >> 1;
+
+    while(tip_bit != 0){
+        sr = IICPtr[0x04 << 1];
+        masked_n = sr & mask;
+        tip_bit = masked_n >> 1;
+    }
+
+
+    // write slave to transmit reg for reading
+    IICPtr[0x03 << 1] = 0xA1; // Data written to memory address location
+    IICPtr[0x04 << 1] = 0x91; //STA, WR, IACK
+
+    // check ack
+    sr = IICPtr[0x04 << 1];
+    mask = 1 << 7;
+    masked_n = sr & mask;
+    ack_bit = masked_n >> 7;
+
+    while(ack_bit != 0){
+        sr = IICPtr[0x04 << 1];
+        masked_n = sr & mask;
+        ack_bit = masked_n >> 7;
+    }
+
+    // check tip flag
+    sr = IICPtr[0x04 << 1];
+    mask = 1 << 1;
+    masked_n = sr & mask;
+    tip_bit = masked_n >> 1;
+
+    while(tip_bit != 0){
+        sr = IICPtr[0x04 << 1];
+        masked_n = sr & mask;
+        tip_bit = masked_n >> 1;
+    }
+
+    //Read from reg
+    IICPtr[0x04 << 1] = 0x69; //RD, ACK = 1, STO, IACK
+
+    // check tip flag
+    sr = IICPtr[0x04 << 1];
+    mask = 1 << 1;
+    masked_n = sr & mask;
+    tip_bit = masked_n >> 1;
+
+    while(tip_bit != 0){
+        sr = IICPtr[0x04 << 1];
+        masked_n = sr & mask;
+        tip_bit = masked_n >> 1;
+    }
+
+    //Store transmit register value into variable and print
+    value = IICPtr[0x03 << 1] ;
+    printf( "\r\nValue Read: %.4x", value ) ;
+}
+
+void IIC_ReadBytes(void){
+    int i;
+    int sr;
+    unsigned int value ;
+    int mask, masked_n, tip_bit, ack_bit;
+    unsigned char *IICPtr = (unsigned char *)(IICStart);
+
+    // check if TIP flag is negated
+    sr = IICPtr[0x04 << 1];
+    mask = 1 << 1;
+    masked_n = sr & mask;
+    tip_bit = masked_n >> 1;
+
+    while(tip_bit != 0){
+        sr = IICPtr[0x04 << 1];
+        masked_n = sr & mask;
+        tip_bit = masked_n >> 1;
+    }
+
+    // Writing to the transmit reg
+    IICPtr[0x03 << 1] = 0xA0;
+    IICPtr[0x04 << 1] = 0x91;
+
+    // check ack
+    sr = IICPtr[0x04 << 1];
+    mask = 1 << 7;
+    masked_n = sr & mask;
+    ack_bit = masked_n >> 7;
+
+    while(ack_bit != 0){
+        sr = IICPtr[0x04 << 1];
+        masked_n = sr & mask;
+        ack_bit = masked_n >> 7;
+    }
+
+    // check tip flag
+    sr = IICPtr[0x04 << 1];
+    mask = 1 << 1;
+    masked_n = sr & mask;
+    tip_bit = masked_n >> 1;
+
+    while(tip_bit != 0){
+        sr = IICPtr[0x04 << 1];
+        masked_n = sr & mask;
+        tip_bit = masked_n >> 1;
+    }
+
+    // specify the internal address
+    // by writing each byte of the addr to the transmit reg
+    IICPtr[0x03 << 1] = 0x00;
+    IICPtr[0x04 << 1] = 0x11; // set WR, IACK
+
+    // check ack
+    sr = IICPtr[0x04 << 1];
+    mask = 1 << 7;
+    masked_n = sr & mask;
+    ack_bit = masked_n >> 7;
+
+    while(ack_bit != 0){
+        sr = IICPtr[0x04 << 1];
+        masked_n = sr & mask;
+        ack_bit = masked_n >> 7;
+    }
+
+    // check tip flag
+    sr = IICPtr[0x04 << 1];
+    mask = 1 << 1;
+    masked_n = sr & mask;
+    tip_bit = masked_n >> 1;
+
+    while(tip_bit != 0){
+        sr = IICPtr[0x04 << 1];
+        masked_n = sr & mask;
+        tip_bit = masked_n >> 1;
+    }
+
+    // second byte of addr
+    IICPtr[0x03 << 1] = 0x00;
+    IICPtr[0x04 << 1] = 0x11; // set WR, IACK
+
+    // check ack
+    sr = IICPtr[0x04 << 1];
+    mask = 1 << 7;
+    masked_n = sr & mask;
+    ack_bit = masked_n >> 7;
+
+    while(ack_bit != 0){
+        sr = IICPtr[0x04 << 1];
+        masked_n = sr & mask;
+        ack_bit = masked_n >> 7;
+    }
+
+    // check tip flag
+    sr = IICPtr[0x04 << 1];
+    mask = 1 << 1;
+    masked_n = sr & mask;
+    tip_bit = masked_n >> 1;
+
+    while(tip_bit != 0){
+        sr = IICPtr[0x04 << 1];
+        masked_n = sr & mask;
+        tip_bit = masked_n >> 1;
+    }
+
+    // check tip flag
+    sr = IICPtr[0x04 << 1];
+    mask = 1 << 1;
+    masked_n = sr & mask;
+    tip_bit = masked_n >> 1;
+
+    while(tip_bit != 0){
+        sr = IICPtr[0x04 << 1];
+        masked_n = sr & mask;
+        tip_bit = masked_n >> 1;
+    }
+
+    // write slave to transmit reg for reading
+    IICPtr[0x03 << 1] = 0xA1; // Data written to memory address location
+    IICPtr[0x04 << 1] = 0x91; //STA, WR, IACK
+
+    // check ack
+    sr = IICPtr[0x04 << 1];
+    mask = 1 << 7;
+    masked_n = sr & mask;
+    ack_bit = masked_n >> 7;
+
+    while(ack_bit != 0){
+        sr = IICPtr[0x04 << 1];
+        masked_n = sr & mask;
+        ack_bit = masked_n >> 7;
+    }
+
+    // check tip flag
+    sr = IICPtr[0x04 << 1];
+    mask = 1 << 1;
+    masked_n = sr & mask;
+    tip_bit = masked_n >> 1;
+
+    while(tip_bit != 0){
+        sr = IICPtr[0x04 << 1];
+        masked_n = sr & mask;
+        tip_bit = masked_n >> 1;
+    }
+
+    for(i = 0; i < 128; i++){
+
+        // if last iteration, need to set sto bit and ACK bit
+        if(i == 127){
+            IICPtr[0x04 << 1] = 0x69; //RD, ACK = 1, STO, IACK
+        }
+        else{
+            IICPtr[0x04 << 1] = 0x21; //RD, ACK = 0, IACK
+        }
+
+        // if not on the last iteration, check ack
+        if(i < 127){
+            sr = IICPtr[0x04 << 1];
+            mask = 1 << 7;
+            masked_n = sr & mask;
+            ack_bit = masked_n >> 7;
+
+            while(ack_bit != 0){
+                sr = IICPtr[0x04 << 1];
+                masked_n = sr & mask;
+                ack_bit = masked_n >> 7;
+            }
+        }
+
+        // check tip flag
+        sr = IICPtr[0x04 << 1];
+        mask = 1 << 1;
+        masked_n = sr & mask;
+        tip_bit = masked_n >> 1;
+
+        while(tip_bit != 0){
+            sr = IICPtr[0x04 << 1];
+            masked_n = sr & mask;
+            tip_bit = masked_n >> 1;
+        }
+
+        //Store transmit register value into variable and print
+        value = IICPtr[0x03 << 1] ;
+        printf( "\r\nValue Read: %.4x", value ) ;
+    }
+}
+
 void IIC_WriteByte(void){
     int sr;
     int mask, masked_n, tip_bit, ack_bit;
