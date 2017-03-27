9127b3a2451394f1a10d79f19bd0dfa4c9df2163
Amro Anwer <amro_anwer41@hotmail.com>
2017-03-26 21:27:29 -0700

    whatever 2.0

--- ADC_Write.c
+++ ADC_Write.c
@@ -17,10 +17,11 @@ void ADC_Write(void){
     }
 
     // Writing to the transmit reg
-    IICPtr[0x03 << 1] = 0x92; // 10010010
     IICPtr[0x04 << 1] = 0x91; // lines 20 and 21 might need to be flipped since they specified that
 															// the address is sent after the start condition
 
+		IICPtr[0x03 << 1] = 0x92; // 10010010
+    
     // check ack
     sr = IICPtr[0x04 << 1];
     mask = 1 << 7;
@@ -84,31 +85,31 @@ void ADC_Write(void){
             IICPtr[0x04 << 1] = 0x11;
         }*/
 				
-	IICPtr[0x04 << 1] = 0x11;
+		IICPtr[0x04 << 1] = 0x11;
 
         // check ack (done in every iteration of loop)
-        sr = IICPtr[0x04 << 1];
-        mask = 1 << 7;
-        masked_n = sr & mask;
-        ack_bit = masked_n >> 7;
+		sr = IICPtr[0x04 << 1];
+		mask = 1 << 7;
+		masked_n = sr & mask;
+		ack_bit = masked_n >> 7;
 
-        while(ack_bit != 0){
-            sr = IICPtr[0x04 << 1];
-            masked_n = sr & mask;
-            ack_bit = masked_n >> 7;
-        }
+		while(ack_bit != 0){
+				sr = IICPtr[0x04 << 1];
+				masked_n = sr & mask;
+				ack_bit = masked_n >> 7;
+		}
 
-        // check tip
-	sr = IICPtr[0x04 << 1];
-	mask = 1 << 1;
-	masked_n = sr & mask;
-	tip_bit = masked_n >> 1;
+		// check tip
+		sr = IICPtr[0x04 << 1];
+		mask = 1 << 1;
+		masked_n = sr & mask;
+		tip_bit = masked_n >> 1;
 
-	while(tip_bit != 0){
-	sr = IICPtr[0x04 << 1];
-	masked_n = sr & mask;
-	tip_bit = masked_n >> 1;
-	}
-    }
+		while(tip_bit != 0){
+		sr = IICPtr[0x04 << 1];
+		masked_n = sr & mask;
+		tip_bit = masked_n >> 1;
+		}
+  }
 }
     
